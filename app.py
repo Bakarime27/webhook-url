610fe02607408520b87def25b6a0bbfabcf6c3ef
Bakarime27 <108431397+Bakarime27@users.noreply.github.com>
2024-06-06 05:43:56 +0200

    Update app.py code to add a queue

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 from flask import Flask, request, jsonify
-import json
+from queue import Queue
+import threading
 
 app = Flask(__name__)
-received_data = {}
+data_queue = Queue()
 
 @app.route("/")
 def index():
-    return "Welcome to the url used as webhook receiver!"
+    return "Welcome to the URL used as a webhook receiver!"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    global received_data
     data = request.json
-    received_data = data
-    print(f"Received data: {data}")
+    data_queue.put(data)
     return '', 200
 
 @app.route('/show_json', methods=['GET'])
 def show_json():
-    global received_data
-    # Copy the current data to return it
-    data_to_return = received_data.copy()
-    # Clear the received_data dictionary
-    received_data.clear()
-    return jsonify(data_to_return)
+    if data_queue.empty():
+        return jsonify({"message": "No data available"}), 200
+    else:
+        data_to_return = data_queue.get()
+        return jsonify(data_to_return)
 
 if __name__ == '__main__':
     app.run(port=5000)
+
